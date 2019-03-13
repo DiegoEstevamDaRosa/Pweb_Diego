e46d2b9eabf2e2635fddaf2b0d0979872ff41051
DiegoEstevamDaRosa <diego.rosa8@unisul.br>
2019-03-13 20:13:05 -0300

    Projeto criado

---
+++ src/main/java/br/unisul/pweb/diego/PwebDiegoApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.pweb.diego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebDiegoApplication.class, args);
+	}
+
+}
